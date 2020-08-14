699788b7b5f86a7eb65e8138ed771b1ae454047f
louisa-sophie <louisa_sophie_1@web.de>
2020-08-14 21:58:42 +0300

    Tree excercise stem added

--- src/main/scala/Loop_Tree_1.scala
+++ src/main/scala/Loop_Tree_1.scala
@@ -1,13 +1,19 @@
 case object Loop_Tree_1 extends App{
 
 println(" "*19 + "**")
-println(" "*19 + "**")
 
     for (i <- 2 to 20)
 {
-  println(" "*(20-i) + "T"*i*2)
+  println(" "*(20-i) + "X"*i*2)
 
   }
+
+    println (" "*15 + "T"*10 )
+    println (" "*15 + "T"*10 )
+    println (" "*15 + "T"*10 )
+
+
+
 
 
 }
